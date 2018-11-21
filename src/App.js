4e5c94b04cd50b5f159d3e71084430bc882a7102
John Flynn <jflynnpics@gmail.com>
2018-11-21 14:45:40 -0500

    start of blackjack

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <body>
+          <header>
+            <title>Blackjack</title>
+            <img class="title" src="images/blackjack name.jpg" height="100" />
+          </header>
+          <section class="hands">
+            <ul class="player" />
+            <button class="hit">hit</button> <button class="stay">stay</button>
+            <section id="playerScore">0</section>
+            <ul class="dealer" />
+            <section id="dealerScore">0</section>
+          </section>
+        </body>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
