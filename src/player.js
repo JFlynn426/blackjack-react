0879080e614f699aa7c388c0f3595db8c135f6f1
John Flynn <jflynnpics@gmail.com>
2018-11-26 11:03:11 -0500

    11-26-18

---
+++ src/player.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import deck from './deck';
+
+class player extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      playerHand: []
+    }
+  }
+}
+  const dealCardToPlayer = () => {
+    let card = deck.pop()
+    playerHand.push(card)
+  }
+  render() {
+    return (
+      <div>
+        <ul class="player" />
+        <button class="hit">hit</button> <button class="stay">stay</button>
+            <section id="playerScore">0</section>
+      </div>
+    );
+  }
+
+export default player;
